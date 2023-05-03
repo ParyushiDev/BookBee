ee09bc1b3a4be90109aa2c54d72ccb06864f3c33
Shinchan09 <92013842+ParyushiDev@users.noreply.github.com>
2023-05-03 13:27:50 +0530

    Add files via upload

---
+++ Bookbee/BookBee/app/src/main/java/com/example/bookbee/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.bookbee
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookbee.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    // binding
+
+    private lateinit var binding:ActivityMainBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //handle login click
+        binding.loginBtn.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        //handle skip click
+        binding.skipBtn.setOnClickListener{
+            startActivity(Intent(this, DashboardUserActivity::class.java))
+        }
+
+
+    }
+}
